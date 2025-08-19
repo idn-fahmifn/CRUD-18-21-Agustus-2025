7869d38b508edafb0dd0e61fd997ba881caa9ab4
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-19 17:01:57 +0700

    2.2

---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple CRUD IDN</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <section id="navbar">
+        <nav class="navbar navbar-expand-lg bg-warning">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Simple CRUD</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tempat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Barang</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section id="body">
+        <div class="container">
+            <div class="card mt-4 p-4">
+                <div class="card-title h5">Judul Halaman</div>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, enim.</span>
+            </div>
+            <div class="card mt-2 p-4">
+                <div class="d-flex justify-content-between">
+                    <div class="">
+                        <div class="card-title h6">Halaman data</div>
+                    </div>
+                    <div class="">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">
+                            Tambah Data
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
